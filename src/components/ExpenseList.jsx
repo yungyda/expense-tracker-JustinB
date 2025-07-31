d84a488639036315c6dfae35e9fe57d9b7ee661c
yungyda <justinbailey821@gmail.com>
2025-07-30 21:40:28 -0500

    Your message here

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 
 // Component to display the list of expenses
-function ExpenseList({ expenses }) {
+function ExpenseList({
+  expenses,
+  filterText,
+  onDeleteExpense,
+  onFilterTextChange,
+}) {
   // Sort expenses by amount (descending order)
   const sortedExpenses = expenses.sort((a, b) => b.amount - a.amount);
 
   return (
- 
-   
-         <table className="expense-table"> 
+    <>
+      <div className="filter-text-wrapper">
+        <span className="filter-text-label">Filter by Description:</span>
+        <input type="text" value={filterText} onChange={onFilterTextChange} />
+      </div>
+      <table className="expense-table">
         <tr>
-          <th>
-            Category
-          </th>
-          <th>
-            Description
-          </th>
-          <th>
-            Amount
-          </th>
+          <th>Category</th>
+          <th>Description</th>
+          <th>Amount</th>
         </tr>
         {sortedExpenses.map((expense) => (
-          <tr key={expense.id} >
+          <tr key={expense.id}>
             <td>{expense.category}</td>
             <td>{expense.description}</td>
-            <td>{expense.amount}{expense.amount >100 && <span className="overspent">⚠️ Overspending!</span>}</td>
+            <td>
+              {expense.amount}
+              {expense.amount > 100 && (
+                <span className="overspent">⚠️ Overspending!</span>
+              )}
+            </td>
+            <td>
+              <button className= "expense-delete-button" onClick={() => onDeleteExpense(expense)}>Delete</button>
+            </td>
           </tr>
         ))}
-       
       </table>
-     
+    </>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import ExpenseForm from "./ExpenseForm";
 import ExpenseList from "./ExpenseList";
 import TotalSpent from "./TotalSpent";
@@ -7,18 +7,51 @@ import CategoryChart from "./CategoryChart";
 // Main dashboard page for managing expenses
 function Home() {
   const [expenses, setExpenses] = useState([]);
+  const [filterText, setFilterText] = useState("");
+
+  const handleFilterTextChange = (event) => {
+    setFilterText(event.target.value);
+  };
 
   // Adds a new expense to the state
   const handleAddExpense = (newExpense) => {
     setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
   };
 
+  //Deletes an expense from the state
+  const handleDeleteExpense = (expense) => {
+    const confirmWithUser = confirm(
+      "Are you sure you want to delete this expense?"
+    );
+    if (!confirmWithUser) return;
+
+    const expenseId = expense.id;
+    setExpenses((expenses) => {
+      const filteredExpenses = expenses.filter((e) => e.id !== expenseId);
+      return [...filteredExpenses];
+    });
+  };
+
+  const filteredExpenses = useMemo(() => {
+    const filterTextLC = filterText.toLowerCase();
+
+    return expenses.filter((e) => {
+      const descriptionLC = e.description.toLowerCase();
+      return descriptionLC.indexOf(filterTextLC) > -1;
+    });
+  }, [expenses, filterText]);
+
   return (
     <div className="home-container">
       <ExpenseForm onAddExpense={handleAddExpense} />
       <TotalSpent expenses={expenses} />
       <CategoryChart expenses={expenses} />
-      <ExpenseList expenses={expenses} />
+      <ExpenseList
+        expenses={filteredExpenses}
+        filterText={filterText}
+        onDeleteExpense={handleDeleteExpense}
+        onFilterTextChange={handleFilterTextChange}
+      />
     </div>
   );
 }
